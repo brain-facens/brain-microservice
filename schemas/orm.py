c807a8b204cfebc9756cd01951b81feccc7fedbc
L0G1C06 <justgithub70@gmail.com>
2023-11-28 16:05:56 -0300

    delete orm schema

--- schemas/orm.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel 
-
-class Insert(BaseModel):
-    table: str 
-    username: str 
-    email: str 
-    password: str 
-    contracted_project: str 
-    company: str 
